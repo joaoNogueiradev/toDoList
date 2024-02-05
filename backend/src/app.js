6c6d99d701213b664fb9723d5a4b2bf52ede941b
João Luiz Lima Nogueira <devjoaoluiz@gmail.com>
2024-02-05 17:30:31 -0300

    Using docker to create a container for mysql database and creation of models,tasks,enviroment variables

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const router = require("./router");
 
 const app = express();
-
-app.get("/", (request, response) => response.status(200).send("Hello, World!"));
+app.use(router);
 
 module.exports = app;
